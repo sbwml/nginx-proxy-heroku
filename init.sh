4799d93e94e15f97406631335c8c2639d74b4018
sbwml <984419930@qq.com>
2021-09-18 14:20:41 +0800

    Host

--- init.sh
+++ init.sh
@@ -6,7 +6,12 @@ server {
     server_name   _;
 
     location / {
+        proxy_set_header Host $HOST;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_redirect off;
         proxy_pass $TARGET;
+        proxy_connect_timeout 60s;
     }
 }
 EOF
