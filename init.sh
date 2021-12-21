afee847c1febc5795177fdbdb5d36b33babb14d6
sbwml <admin@cooluc.com>
2021-12-21 17:26:38 +0800

    Add proxy_set_header Referer

--- init.sh
+++ init.sh
@@ -7,10 +7,13 @@ server {
 
     location / {
         proxy_set_header Host $HOST;
+        proxy_set_header Referer $REFERER;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_redirect off;
         proxy_pass $TARGET;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
         proxy_connect_timeout 60s;
     }
 }
